eac8bc64eeaf2ed518e7985fb88d4cbb8784b0b7
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-06-03 01:54:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	let submit=document.getElementByTag('input')[2];
+	submit.addEventListener("click",()=>{
+		let firstName=document.getElementByTag('input')[0];
+		let LastName=document.getElementByTag('input')[1];
+		alert(firstName+" "+LastName);
+	})
 
 }
